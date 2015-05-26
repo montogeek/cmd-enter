f3373e8ae7f592378be2ef690748f50089593b0a
Fernando Montoya <montogeek@gmail.com>
2015-05-25 23:57:50 -0500

    Initial

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict'
-(function() {
 
-})()
+function CmdEnter(form, textarea) {
+
+  var formElement = document.querySelector(form);
+  var textareaElement = document.querySelector(textarea);
+
+  // Check compatibility
+  textareaElement.addEventListener('keyup', function(event) {
+    // Check for metaKey, should be compatible with Windows, Linux and MacOSX.
+    if(event.keyCode === 13) {
+      formElement.submit();
+    }
+  });
+}
+
+module.exports = CmdEnter;
